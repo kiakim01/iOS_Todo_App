81db43078598696e0ffad0446010155201a6c81e
kiakim01 <hnmpot0318@naver.com>
2023-08-07 20:11:08 +0900

    Try Text Field

--- iOS_Memo/ToDo/TodoListPage.swift
+++ iOS_Memo/ToDo/TodoListPage.swift
@@ -6,7 +6,6 @@ class TodoListPage: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 뷰 컨트롤러 초기화 작업 수행
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -16,54 +15,40 @@ class TodoListPage: UIViewController {
 extension TodoListPage: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // 실제 데이터의 행 수 반환
         return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath)
         
-        cell.textLabel?.text = "\(indexPath.row + 1)"
+        // Find the existing text field in the cell or create a new one
+        var textField = cell.viewWithTag(123) as? UITextField
+        if textField == nil {
+            textField = UITextField(frame: cell.contentView.bounds)
+            textField?.placeholder = "Enter text"
+            textField?.tag = 123
+            textField?.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+            cell.contentView.addSubview(textField!)
+        }
+        
+        textField?.text = "\(indexPath.row + 1)"
         
         return cell
     }
-    //New
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // 셀이 선택되었을 때 호출되는 메서드
-        showTextAlert(for: indexPath)
-    }
-    
-    // 사용자 정의 함수: 텍스트 입력을 받는 알림창을 표시하는 메서드
-    func showTextAlert(for indexPath: IndexPath) {
-        let alertController = UIAlertController(title: "텍스트 입력", message: "원하는 텍스트를 입력하세요.", preferredStyle: .alert)
-        
-        alertController.addTextField { textField in
-            textField.placeholder = "텍스트 입력"
-        }
-        
-        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-        let saveAction = UIAlertAction(title: "저장", style: .default) { [weak self, weak alertController] _ in
-            guard let textField = alertController?.textFields?.first,
-                  let newText = textField.text else {
-                return
-            }
-            // 입력받은 텍스트를 셀에 적용
-            self?.updateText(newText, for: indexPath)
-        }
-        
-        alertController.addAction(cancelAction)
-        alertController.addAction(saveAction)
-        
-        present(alertController, animated: true, completion: nil)
-    }
-    
+
     // 사용자 정의 함수: 텍스트를 셀에 업데이트하는 메서드
     func updateText(_ newText: String, for indexPath: IndexPath) {
-        guard let cell = tableView.cellForRow(at: indexPath) else {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.textLabel?.text = newText
+        }
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        guard let text = textField.text,
+              let row = textField.tag as Int? else {
             return
         }
-        cell.textLabel?.text = newText
+        updateText(text, for: IndexPath(row: row, section: 0))
     }
 }
-
 
