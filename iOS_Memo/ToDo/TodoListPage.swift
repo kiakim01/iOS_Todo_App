f8dca0dbb0c75d9e0be12c43ae99eee1fe72a86b
kiakim01 <hnmpot0318@naver.com>
2023-08-09 15:18:19 +0900

    Add Switch Toggle

--- iOS_Memo/ToDo/TodoListPage.swift
+++ iOS_Memo/ToDo/TodoListPage.swift
@@ -4,6 +4,23 @@ class TodoListPage: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var numberOfItems = 2 // 초기 행 개수
+    var switchStates: [IndexPath: Bool] = [:] // 각 셀의 UISwitch 상태를 저장하는 딕셔너리
+
+    
+    @IBAction func TodoButton(_ sender: UIButton) {
+        // TodoButton에 대한 액션 구현
+    }
+    
+    @IBAction func CompleteButton(_ sender: UIButton) {
+        // CompleteButton에 대한 액션 구현
+    }
+    
+    @IBAction func AddButton(_ sender: UIButton) {
+        numberOfItems += 1
+        tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -15,28 +32,38 @@ class TodoListPage: UIViewController {
 extension TodoListPage: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return numberOfItems
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath)
         
-        // Find the existing text field in the cell or create a new one
         var textField = cell.viewWithTag(123) as? UITextField
         if textField == nil {
-            textField = UITextField(frame: cell.contentView.bounds)
-            textField?.placeholder = "Enter text"
+            textField = UITextField(frame: CGRect(x: 0, y: 0, width: cell.contentView.bounds.width - 80, height: cell.contentView.bounds.height))
+            textField?.placeholder = "텍스트 입력"
             textField?.tag = 123
             textField?.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
             cell.contentView.addSubview(textField!)
         }
         
-        textField?.text = "\(indexPath.row + 1)"
+        //UI에 토글 스위치 추가
+        let toggle = UISwitch()
+        toggle.isOn = switchStates[indexPath] ?? false
+        toggle.onTintColor = .blue
+        toggle.addTarget(self, action: #selector(switchDidChange(_:)), for: .valueChanged)
+        
+  // UISwitch의 위치를 계산합니다.
+        toggle.frame.origin = CGPoint(x: cell.contentView.bounds.width - toggle.bounds.width - 8, y: (cell.contentView.bounds.height - toggle.bounds.height) / 2)
+        cell.contentView.addSubview(toggle)
         
         return cell
     }
-
-    // 사용자 정의 함수: 텍스트를 셀에 업데이트하는 메서드
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
     func updateText(_ newText: String, for indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath) {
             cell.textLabel?.text = newText
@@ -49,6 +76,11 @@ extension TodoListPage: UITableViewDelegate, UITableViewDataSource {
             return
         }
         updateText(text, for: IndexPath(row: row, section: 0))
+    }
+    
+   // 토글 액션추가
+    @objc func switchDidChange(_ toggleSwitch: UISwitch) {
+        print("Toggle state changed: \(toggleSwitch.isOn)")
     }
 }
 
